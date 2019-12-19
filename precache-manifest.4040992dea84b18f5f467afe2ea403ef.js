ff99e2aab7f4e14d41bd5f5dbb75d193c1e6324a
kimjunyeob <tislwlstnf1@naver.com>
2019-12-20 00:14:30 +0900

    Updates

---
+++ precache-manifest.4040992dea84b18f5f467afe2ea403ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55819b9b87008b15f616d45206b91ad4",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "efc52f2148de87ffdc2c",
+    "url": "/movie_app_2019/static/css/main.b276d142.chunk.css"
+  },
+  {
+    "revision": "740f30b1bc8aae6bfc8c",
+    "url": "/movie_app_2019/static/js/2.54ae61fe.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/movie_app_2019/static/js/2.54ae61fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efc52f2148de87ffdc2c",
+    "url": "/movie_app_2019/static/js/main.5f0e3685.chunk.js"
+  },
+  {
+    "revision": "101d7acd549509bcf93b",
+    "url": "/movie_app_2019/static/js/runtime-main.69d003e7.js"
+  }
+]);
